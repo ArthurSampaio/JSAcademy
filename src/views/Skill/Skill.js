2f5dbfb7f20d46f74bad4394bba1ede522530120
Arthur Sampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2019-04-24 14:24:38 -0300

    Do request from page

--- src/views/Skill/Skill.js
+++ src/views/Skill/Skill.js
@@ -1,24 +1,62 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from 'classnames'
 // react components for routing our app without refresh
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
+import withStyles from '@material-ui/core/styles/withStyles'
 // @material-ui/icons
 // core components
-import Header from "components/Header/Header.jsx";
-import Footer from "components/Footer/Footer.jsx";
+import Header from 'components/Header/Header.jsx'
+import Footer from 'components/Footer/Footer.jsx'
+import TerminalJS from 'components/TerminalJS/TerminalJS'
+import Button from 'components/CustomButtons/Button.jsx'
 
 // sections for this page
-import HeaderLinks from "components/Header/HeaderLinks.jsx";
-import SectionBasics from "./Sections/SectionBasics";
+import HeaderLinks from 'components/Header/HeaderLinks.jsx'
+import SectionBasics from './Sections/SectionBasics'
+import LessonAPI from '../../services/LessonAPI'
 
-import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
+import componentsStyle from 'assets/jss/material-kit-react/views/components.jsx'
 
-const Skill = (props) => {
+const Skill = props => {
+  const { classes, ...rest } = props
+  const [task, setTask] = useState({})
+  const [func, setFunc] = useState(' ')
+  const [lesson, setLesson] = useState({})
+  const [order, setOrder] = useState(0)
 
-  const { classes, ...rest } = props;
+  useEffect(() => {
+    // Update the document title using the browser API
+    const fetchData = async () => {
+      const res = await getLesson()
+      setLesson(res)
+      setTask(res.exercises[order])
+      setFunc(res.exercises[order].appraisedFunction)
+    }
+    fetchData()
+  }, [func, order])
+
+  function getLesson() {
+    const { match } = props
+    return LessonAPI.getLessonById(match.params.lessonId)
+  }
+
+  function nextExercise() {
+    const newOrder = order + 1 < lesson.exercises.length ? order + 1 : order
+    setOrder(newOrder)
+  }
+
+  function previousExercise() {
+    const newOrder = order - 1 >= 0 ? order - 1 : order
+    setOrder(newOrder)
+  }
+
+  function handleChange(value) {
+    const help = value ? nextExercise : console.log
+    help()
+  }
+
   return (
     <div>
       <Header
@@ -31,14 +69,33 @@ const Skill = (props) => {
 
       <div className={classNames(classes.main, classes.mainRaised)}>
         <div className={classes.container}>
-          <div className={classes.brand}>
+          <div className={classes.brand} />
+          <div className={classes.sections}>
+            <div className={classes.container}>
+              <TerminalJS task={task} onRun={handleChange} />
+            </div>
+            <Button
+              id="run"
+              variant="contained"
+              color="primary"
+              onClick={previousExercise}
+            >
+              previous
+            </Button>
+            <Button
+              id="run"
+              variant="contained"
+              color="primary"
+              onClick={nextExercise}
+            >
+              next
+            </Button>
           </div>
-          <SectionBasics {...props} />
         </div>
       </div>
       <Footer />
     </div>
-  );
+  )
 }
 
-export default withStyles(componentsStyle)(Skill);
+export default withStyles(componentsStyle)(Skill)
