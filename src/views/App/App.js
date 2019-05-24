80da50219fe5cf7e727855d0201c486ab53479c6
Arthur Sampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2019-05-24 16:44:06 -0300

    Add axios interceptor

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Router, Route, Switch } from 'react-router-dom'
+import axios from 'axios'
 
 import Skill from 'views/Skill/Skill'
 import LandingPage from 'views/LandingPage/LandingPage.jsx'
@@ -8,9 +9,22 @@ import LoginPage from 'views/LoginPage/LoginPage.jsx'
 import TerminalJS from 'components/TerminalJS/TerminalJS'
 import RegisterPage from 'views/RegisterPage/RegisterPage'
 import { createBrowserHistory } from 'history'
+
 var hist = createBrowserHistory()
 
 const App = props => {
+  axios.interceptors.request.use(
+    function(config) {
+      // Do something before request is sent
+      console.log('>>>>>>>>>>', config)
+      return config
+    },
+    function(error) {
+      // Do something with request error
+      return Promise.reject(error)
+    }
+  )
+
   function TerminalTest() {
     const testValue = `function a (b) {
             const d = (x) => 2*x
