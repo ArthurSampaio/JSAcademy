549246f2400302eb5b24f8987ac34a70ffc77fe3
Arthur Sampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2019-05-24 16:17:02 -0300

    Rename loginPages's header

--- src/views/LoginPage/LoginPage.jsx
+++ src/views/LoginPage/LoginPage.jsx
@@ -47,7 +47,7 @@ class LoginPage extends React.Component {
         <Header
           absolute
           color="transparent"
-          brand="Material Kit React"
+          brand="JavaScript Academy"
           rightLinks={<HeaderLinks />}
           {...rest}
         />
