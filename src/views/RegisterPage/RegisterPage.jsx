fee4d3d47c7ccc4d7d6e75f25e2d5e166c50019e
Arthur Sampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2019-06-13 17:45:55 -0300

    Fix age flow

--- src/views/RegisterPage/RegisterPage.jsx
+++ src/views/RegisterPage/RegisterPage.jsx
@@ -1,32 +1,31 @@
-import React, { useState, Fragment } from 'react';
-import { Link } from "react-router-dom";
+import React, { useState, Fragment } from 'react'
+import { Link } from 'react-router-dom'
 
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from 'classnames'
 // react components for routing our app without refresh
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
+import withStyles from '@material-ui/core/styles/withStyles'
 // @material-ui/icons
 // core components
-import Header from "components/Header/Header.jsx";
-import GridContainer from "components/Grid/GridContainer.jsx";
-import GridItem from "components/Grid/GridItem.jsx";
-import Button from "components/CustomButtons/Button.jsx";
-import Parallax from "components/Parallax/Parallax.jsx";
+import Header from 'components/Header/Header.jsx'
+import GridContainer from 'components/Grid/GridContainer.jsx'
+import GridItem from 'components/Grid/GridItem.jsx'
+import Button from 'components/CustomButtons/Button.jsx'
+import Parallax from 'components/Parallax/Parallax.jsx'
 // sections for this page
-import HeaderLinks from "components/Header/HeaderLinks.jsx";
-import FiberManualRecord from "@material-ui/icons/FiberManualRecord";
+import HeaderLinks from 'components/Header/HeaderLinks.jsx'
+import FiberManualRecord from '@material-ui/icons/FiberManualRecord'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
 
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Radio from "@material-ui/core/Radio";
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Radio from '@material-ui/core/Radio'
 
+import componentsStyle from 'assets/jss/material-kit-react/views/register.jsx'
 
-import componentsStyle from "assets/jss/material-kit-react/views/register.jsx";
-
-const RegisterPage = (props) => {
-
-  const { classes, ...rest } = props;
-  const [selectRadio, setSelectRadio] = useState("a")
+const RegisterPage = props => {
+  const { classes, ...rest } = props
+  const [selectRadio, setSelectRadio] = useState('a')
 
   function onChangeRadio(onchange) {
     console.log(onchange.target.value)
@@ -34,10 +33,10 @@ const RegisterPage = (props) => {
   }
 
   const dailyMinutes = [
-    { value: "a", text: "Melhor que nada", time: "5" },
-    { value: "b", text: "De boa", time: "10" },
-    { value: "c", text: "Instigado", time: "15" },
-    { value: "d", text: "Sangue nos olhos", time: "20" }
+    { value: 'a', text: 'Melhor que nada', time: '5' },
+    { value: 'b', text: 'De boa', time: '10' },
+    { value: 'c', text: 'Instigado', time: '15' },
+    { value: 'd', text: 'Sangue nos olhos', time: '20' },
   ]
 
   return (
@@ -49,61 +48,34 @@ const RegisterPage = (props) => {
         color="transparent"
         {...rest}
       />
-      <Parallax image={require("assets/img/register-bg.png")}>
+      <Parallax image={require('assets/img/register-bg.png')}>
         <div className={classes.container}>
           <div className={classes.brand}>
             <h3 className={classes.title}>
-              Ótimo! Agora escolha sua meta diária.
+              Ótimo! O que você quer fazer hoje?
             </h3>
-            <GridContainer>
-              <GridItem >
-                {dailyMinutes.map(item => (
-                  <Fragment key={item.value} className={classes.contentAlign}>
-                    <div
-
-                    >
-                      <FormControlLabel
-                        control={
-                          <Radio
-                            checked={selectRadio === item.value}
-                            onChange={onChangeRadio}
-                            value={item.value}
-                            name="radio button enabled"
-                            aria-label="A"
-                            icon={
-                              <FiberManualRecord
-                                className={classes.radioUnchecked}
-                              />
-                            }
-                            checkedIcon={
-                              <FiberManualRecord className={classes.radioChecked} />
-                            }
-                            classes={{
-                              checked: classes.radio
-                            }}
-                          />
-                        }
-                        classes={{
-                          label: classes.label
-                        }}
-                        label={`${item.text} - ${item.time} minutos`}
-                      />
-                    </div>
-                  </Fragment>
-                ))}
-                <Button component={Link} to="/skill/5cc34f366fa1ae44102f7605" className={classes.buttonRegister} id="run" variant="contained" color="primary" >
+            <GridContainer className={classes.containerGrid}>
+              <GridItem>
+                {/* <Button component={Link} to="/skill/5cc34f366fa1ae44102f7605" className={classes.buttonRegister} id="run" variant="contained" color="primary" >
                   Definir Meta.
-                </Button>
+                </Button> */}
+                <ButtonGroup
+                  fullWidth
+                  aria-label="group button"
+                  color="primary"
+                >
+                  <Button className={classes.buttonChoices}>
+                    Quero criar uma lista de exercícios
+                  </Button>
+                  <Button>Quero responder uma lista de exercícios</Button>
+                </ButtonGroup>
               </GridItem>
-
             </GridContainer>
           </div>
-
         </div>
       </Parallax>
     </div>
-  );
+  )
 }
 
-
-export default withStyles(componentsStyle)(RegisterPage);
+export default withStyles(componentsStyle)(RegisterPage)
